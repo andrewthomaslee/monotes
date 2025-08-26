6f9bcb84c627a1686e2b7382d286d94b977f334a
Andrew Thomas Lee <andrewthomaslee.business@gmail.com>
2025-08-25 20:28:39 -0500

    --minify

--- flake.nix
+++ flake.nix
@@ -148,7 +148,7 @@
         installPhase = ''
           mkdir -p $out/style
           cp -r ${./style}/* $out/style/
-          ${pkgs.tailwindcss_4}/bin/tailwindcss -i ${./style/input.css} -o ./style/output.css
+          ${pkgs.tailwindcss_4}/bin/tailwindcss -i ${./style/input.css} -o ./style/output.css --minify
           cp ./style/output.css $out/style/output.css
           cp ${./main.py} $out/main.py
           chmod +x $out/main.py
